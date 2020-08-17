574434fa1acfcb1a798e02578be790e464652098
Jack Hruska <jph239@cornell.edu>
2020-08-17 18:48:29 -0500

    Update 01_repeat_database.sh

--- Repeats/Custom_Library/01_repeat_database.sh
+++ Repeats/Custom_Library/01_repeat_database.sh
@@ -7,4 +7,4 @@
 #$ -pe sm 8
 #$ -P quanah
 
-/home/jmanthey/RepeatModeler-open-1.0.11/BuildDatabase -engine ncbi -name colaptes_hic c05_colaptes_arcs3.FINAL.reordered_2.fasta  
+/home/jmanthey/RepeatModeler-open-1.0.11/BuildDatabase -engine ncbi -name colaptes_hic c05_colaptes_arcs3.fasta  
